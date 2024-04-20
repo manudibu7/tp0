6d17adafe55aef33f0063fcda69bfe2ea4036e6e
utnso <mdibucci@frba.utn.edu.ar>
2024-04-20 07:41:33 +0000

    Resuelvo etapa 1 2 y 3

--- client/src/client.c
+++ client/src/client.c
@@ -1,4 +1,5 @@
 #include "client.h"
+#include <readline/readline.h>
 
 int main(void)
 {
@@ -18,6 +19,7 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
+	log_info(logger,"Hola! Soy un log");
 
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
@@ -26,8 +28,13 @@ int main(void)
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
 
 	// Loggeamos el valor de config
+	log_info(logger, "VALOR leido de la config: %s", valor);
+
 
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
@@ -42,26 +49,31 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor,conexion);
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
 
-	terminar_programa(conexion, logger, config);
-
+	terminar_programa(conexion,logger,config);
 	/*---------------------------------------------------PARTE 5-------------------------------------------------------------*/
 	// Proximamente
+	printf("\nCLIENTE CERRADO");
 }
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("tp0.log","LOGGER_TP0",1,LOG_LEVEL_INFO);
 
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config;
+	t_config* nuevo_config = config_create("/home/utnso/tp0/client/cliente.config");
+	if(nuevo_config==NULL){
+		perror("Error al intentar cargar el config");
+		exit(EXIT_FAILURE);
+	}
 
 	return nuevo_config;
 }
@@ -72,29 +84,48 @@ void leer_consola(t_log* logger)
 
 	// La primera te la dejo de yapa
 	leido = readline("> ");
-
+	log_info(logger,">> %s", leido);
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
+	while((strcmp(leido,"")!=0)){
+		free(leido);
+		leido = readline("> ");
+		log_info(logger,">> %s", leido);
+	}
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
-
+	free(leido);
 }
 
 void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
-	char* leido;
-	t_paquete* paquete;
+	char* leido = NULL;
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
-
+	leido = readline("> ");
+	while((strcmp(leido,"")!=0)){
+		agregar_a_paquete(paquete,leido,strlen(leido)+1);
+		free(leido);
+		leido = readline("> ");
+		}
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	free(leido);
+
+	//Envio el paquete
+	enviar_paquete(paquete,conexion);
+
+	eliminar_paquete(paquete);
+
 	
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
+	log_destroy(logger);
+	config_destroy(config);
+	liberar_conexion(conexion);
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 }
--- server/src/server.c
+++ server/src/server.c
@@ -3,14 +3,14 @@
 int main(void) {
 	logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
 
-	int server_fd = iniciar_servidor();
-	log_info(logger, "Servidor listo para recibir al cliente");
-	int cliente_fd = esperar_cliente(server_fd);
+	int server_fd = iniciar_servidor(); //inicia servidor en ese fd (file descriptos)
+	log_info(logger, "Servidor listo para recibir al cliente"); //logea
+	int cliente_fd = esperar_cliente(server_fd); //se pone a esperar cliente
 
 	t_list* lista;
 	while (1) {
-		int cod_op = recibir_operacion(cliente_fd);
-		switch (cod_op) {
+		int cod_op = recibir_operacion(cliente_fd); //bloqueante: se queda esperando sin avanzar
+		switch (cod_op) { //recibe cod de operacion q indica si voy a enviar mensaje o paquete
 		case MENSAJE:
 			recibir_mensaje(cliente_fd);
 			break;
